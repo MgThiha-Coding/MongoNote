76d0a0781542e5c0b8440219bc52732b8748783c
Min Thiha Aung <minthihaaung950@gmail.com>
2025-05-05 05:03:58 +0630

    change database

--- lib/repository/constant.dart
+++ lib/repository/constant.dart
@@ -1,17 +1,5 @@
-/*
 class Repository {
   static const MONGO_URL =
       "mongodb+srv://nat183434:VKkouohbkPDbjUuN@cluster0.a619lvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   static const userCollection = "users";
-}
-*/
-class Repository {
-  static String MONGO_URL = '';
-  static String userCollection = '';
-
-  // Method to update the connection string and collection name dynamically
-  static void setConnectionDetails(String uri, String collection) {
-    MONGO_URL = uri;
-    userCollection = collection;
-  }
 }
